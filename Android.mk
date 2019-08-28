75affd9a5b8b8e95b1570d7eb47306979b9b931c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-28 09:52:53 -0300

    Declare priv-app permission
    Change-Id: I948957cfe60812c9c3eb59428459532ea1944a56

--- Android.mk
+++ Android.mk
@@ -8,4 +8,12 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
+LOCAL_REQUIRED_MODULES := org.pixelexperience.screenshot.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := org.pixelexperience.screenshot.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := prebuilt/org.pixelexperience.screenshot.xml
 include $(BUILD_PREBUILT)
